b933f1f44cb441c1dfc1d2759a970d63ebccbcc4
PedroTrudes <pedro.jtrudes@gmail.com>
2025-08-26 17:21:22 -0300

    :sparkles: feat: navigation in componentes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,26 @@
+//Arquivo de estrutura das rotas
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players';
+import { NewGroup } from '@screens/NewGroup';
+
+const NativeStack = createNativeStackNavigator();
+
+export function AppRoutes() {
+    return(
+        <NativeStack.Navigator initialRouteName='groups' screenOptions={{headerShown: false}}>
+            <NativeStack.Screen 
+            name='groups' 
+            component={Groups}/>
+            
+            <NativeStack.Screen 
+            name='new' 
+            component={NewGroup} />
+            
+            <NativeStack.Screen 
+            name='players' 
+            component={Players} />
+        </NativeStack.Navigator>
+    );
+}
+
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Header } from "@components/Header";
+import { useNavigation } from "@react-navigation/native";
 import * as Group from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-export function Groups() {
+type RootParamList = {
+        groups: undefined;
+        new: undefined;
+        players: {
+            group: string;
+    }
+}
+
+type Props = {
+    navigation: NativeStackNavigationProp<RootParamList, 'groups'>;
+}
+
+export function Groups({navigation}: Props) {
     const [groups, setGroups] = useState<string[]>([]);
+    //const navigation = useNavigation();
+
+    function handleNewGroup () {
+        navigation.navigate('new');
+    }
 
     return(
         <Group.Container>
             <Header />
-            <Group.Title>Teste</Group.Title>
+            <Group.Title>Ignite Teams</Group.Title>
             <Highlight title="Turmas" subtitle="Jogue com a sua turma"/>
             <FlatList 
                 data={groups}
@@ -30,6 +49,7 @@ export function Groups() {
             <Button 
             title="Criar nova turma"
             type="PRIMARY"
+            onPress={handleNewGroup}
             />
 
         </Group.Container>
